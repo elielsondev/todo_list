d7f40a6302e5932ec478633452fe3be99d7eb44b
Elielson Nascimento <nascimento.elielson@gmail.com>
2023-12-26 18:20:03 -0300

    style - css and add favicon

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
-console.log('Todo list');
+// const date = document.querySelector('#date').value;
+// const titleTask = document.querySelector('#title-task').value;
+const save = document.querySelector('#save');
+
+save.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log('Clicou!!!');
+});
